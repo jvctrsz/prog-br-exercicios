1040ff80fb9fb3d64149b7a2209321684bf00fec
João Matias <joaosteam26@gmail.com>
2023-01-03 21:50:30 -0300

    operadores

---
+++ exercicios/operadores/sricpt.js
@@ -0,0 +1,23 @@
+
+
+//OPERADORES
+
+
+/*
+modulo faz a divisao e se a divisao nao for perfeita ele retorna o restante
+25 % 25 = 0
+27 % 25 = 2 
+*/
+
+// var a = 27
+// var b = 25
+// var c = a % b;
+// console.log(c)
+// console.log(++a)
+
+
+var a = 150;
+
+a = 5;
+
+console.log(a);
---
+++ exercicios/var/tp-dados/sricpt.js
@@ -0,0 +1,33 @@
+
+y = 2;
+
+var w = 4;
+
+var x = y + w;
+
+
+console.log(x)
+
+
+var frase = 'primeirinha linha \nsegunda linha';
+console.log(frase);
+
+
+
+
+//OPERADORES
+
+
+/*
+modulo faz a divisao e se a divisao nao for perfeita ele retorna o restante
+25 % 25 = 0
+27 % 25 = 2 
+*/
+
+var a = 27
+var b = 25
+var c = a % b;
+console.log(c)
+console.log(++a)
+
+
