f791baaaaebb8e942f226967ba644a05dc53a5ba
João Matias <joaosteam26@gmail.com>
2023-01-03 22:44:38 -0300

    fim dia

--- exercicios/operadores/sricpt.js
+++ exercicios/operadores/sricpt.js
@@ -16,8 +16,39 @@ modulo faz a divisao e se a divisao nao for perfeita ele retorna o restante
 // console.log(++a)
 
 
-var a = 150;
+// var a = 150;
 
-a = 5;
+// a = 5;
 
-console.log(a);
+// console.log(a);
+
+// var idade = 20;
+
+// var entrada = idade >= 18 ? 'Pode entrar' : 'Não pode entrar';
+
+// console.log(entrada);
+
+// var a = true;
+// var b = true;
+// var c = a && b;
+// console.log('a',c)
+
+
+// var a = '10.5';
+// var b = '25';
+// var c = parseFloat(a) + parseInt(b);
+// console.log(c);
+
+
+// var nome = prompt('digite seu texto')
+
+
+// alert('Bem vindo, ' + nome)
+
+
+var numero = parseInt(prompt('Digite seu número '));
+
+var dobro = numero*2;
+
+alert('O dobro do ' + numero + ' é ' + dobro );
+
