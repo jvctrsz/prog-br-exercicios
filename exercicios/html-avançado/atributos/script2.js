5542774d7bc6ffd95d0fbcef8da43a5fadef72ce
João Victor Matias <108106191+jvctrsz@users.noreply.github.com>
2023-01-17 21:18:56 -0400

    fim dia

---
+++ exercicios/html-avançado/atributos/script2.js
@@ -0,0 +1,83 @@
+let tela = document.getElementById('tela');
+
+let ctx = tela.getContext('2d');
+
+
+// animacao com circulo
+// let circle = {
+//     x : 250,
+//     y : 250,
+//     raio : 100,
+//     inicio : 0,
+//     fim : 0,
+// }
+
+
+// function drawCircle(c){
+    
+//     ctx.beginPath();
+//     ctx.rect(0,0, 500,500)
+//     ctx.fillStyle = 'aqua'
+//     ctx.fill()
+
+//     ctx.beginPath();
+//     ctx.strokeStyle = 'red';
+//     ctx.fillStyle = 'blue';
+//     ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim);
+
+//     ctx.stroke();
+//     ctx.fill();
+// }
+
+// setInterval(function () {
+
+//     if(circle.fim < 2 * Math.PI) {
+//         circle.fim += 01;
+//         circle.x += 10;
+//     }
+//     drawCircle(circle)
+// }, 10)
+
+
+
+
+
+// ciclos
+// ctx.beginPath();
+
+// ctx.lineWidth = 4;
+// ctx.strokeStyle = 'red'
+// ctx.moveTo(10,10)
+// ctx.lineTo(400, 300)
+// ctx.lineTo(10, 300)
+// ctx.closePath();
+// ctx.stroke()
+
+
+// ctx.beginPath();
+// ctx.lineWidth = 4;
+// ctx.strokeStyle = 'blue'
+// ctx.fillStyle = 'green'
+// ctx.moveTo(50,10)
+// ctx.lineTo(300,300)
+// ctx.lineTo(50,300)
+// ctx.closePath();
+// ctx.stroke()
+// ctx.fill()
+// ciclos
+
+
+
+// quadrado
+// ctx.rect(10,10,100,200)
+
+// ctx.fillStyle = 'blue';
+// ctx.lineWidth = 5
+// ctx.strokeStyle = 'red';
+
+// ctx.fill();
+// ctx.stroke();
+
+
+// ctx.clearRect(20,20,30,30)
+// quadrado
